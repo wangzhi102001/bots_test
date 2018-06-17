0bee7de2c31aeaf57afa67d1fa6d8ccf0e33e066
wangzhi102001 <wangzhi102001@live.com>
2018-06-17 23:08:14 +0800

    20180617,尚未解决导出乱码问题

--- bots_test/changderibao.py
+++ bots_test/changderibao.py
@@ -4,18 +4,27 @@ import fangfa as ff
 from time import sleep
 import json
 import time
+import requests
 #date = input("请输入日期，格式'20180404'")
-urls_bans =[]
+
+session = requests.Session()
+headers = {
+   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134","Accrpt":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"}
+headers_two = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36","Accrpt":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"}
+urls_bans = []
 urls_news = []
 file_name = 'new.txt'
 json_name = 'new.json'
 date_need = ''
-length =0
+length = 0
 js_list = []
 keyword = ''
-links=[]
+links = []
 b = True
-num =20
+title_one_news = ""
+title_two_news = ""
+content_news = ""
+num = 1
 
 #while True and num>=0:
 #    date_need = ff.getdate(b)
@@ -24,7 +33,7 @@ num =20
 #    if input("是否启用关键字筛选链接（Y/N）").lower()=='y'and b:
 #        keyword =input("请输入关键词")
 #        print(r"%s:正在查询,包含关键词%s的链接"% (date_need,keyword))
-#        links+=ff.screeningUrl(links_before.copy(),keyword)        
+#        links+=ff.screeningUrl(links_before.copy(),keyword)
 #        print(r"%s:共查询出包含关键词%s的链接%d条"% (date_need,keyword,len(links)))
 #    if input("是否使用下一日期查询（Y/N）").lower()=='y'and b:
 #        date_need = ff.date_add(date_need)
@@ -38,37 +47,43 @@ num =20
 #            b = False
 #    if input("是否开始下载（Y/N）").lower()=='y':
 #        break
-while True and num>=0:   
-    date_need = ff.getdate(b)
-    links_before = ff.findWithDate(date_need,urls_bans,urls_news)
-    print(r"%s:共获取,%d条信息链接"% (date_need,len(links_before)))
-    keyword ='城头山'
-    print(r"%s:正在查询,包含关键词%s的链接"% (date_need,keyword))
-    links+=ff.screeningUrl(links_before.copy(),keyword)        
-    print(r"%s:共查询出包含关键词%s的链接%d条"% (date_need,keyword,len(links)))
+date_need = ff.getdate()
+while True and num >= 0:   
+    
+    links_before = ff.findWithDate(date_need,urls_bans,urls_news,headers)
+    print(r"%s:共获取,%d条信息链接" % (date_need,len(links_before)))
+    #keyword ='城头山'
+    #print(r"%s:正在查询,包含关键词%s的链接"% (date_need,keyword))
+    #links+=ff.screeningUrl(links_before.copy(),keyword,headers)
+    #print(r"%s:共查询出包含关键词%s的链接%d条"% (date_need,keyword,len(links)))
+    links = links_before
     #num = 120
     date_need = ff.date_add(date_need)
     num-=1
-    b = False
+    
 
 
 
         
 
-for link in links:
-    content = ff.typeDate(ff.getText(link))
-    if content['bt1']!='':
+for link in links[:5]:
+    
+    if ff.getText(link,headers)['bt1'] != '':
+        content = ff.typeDate(ff.getText(link,headers))
+    else:
+       continue
+    if content['bt1'] != '':
         js_list.append(content)
-        ff.saveToTxt(content,file_name)
+        ff.saveToTxt(content,ff.make_path(file_name,link))
         length+=1
         print('%s已写入' % link)
-        sleep(1)
-    if length>=40:
         sleep(0.5)
+    if length >= 40:
+        sleep(1)
         length = 0
 
-ff.saveToJson(js_list,json_name)
-print('全部完成，共写入%s条新闻信息' % len)
+ff.saveToJson(js_list,ff.make_path(json_name,link))
+print('全部完成，共写入%s条新闻信息' % len(js_list))
 
 
 
--- bots_test/fangfa.py
+++ bots_test/fangfa.py
@@ -6,46 +6,50 @@ import json
 import datetime
 import time
 import re
+import requests
 
-def getLinksFrom_Ban(date,url_ban):
+def getLinksFrom_Ban(date,url_ban,headers):
     '''从BS对象获取整版所有新闻链接尾的列表'''
-    if url_ban!=None:
-        bsObj = getbsObj(url_ban)
-    urls_news_part=[]
+    if url_ban != None:
+        bsObj = getbsObj(url_ban,headers)
+    urls_news_part = []
     if bsObj != None:
         for link in bsObj.findAll("area"):
             if 'href' in link.attrs:
-                urls_news_part.append("http://cdrb.cdyee.com/html/%s-%s/%s/" % (date[:4],date[4:6],date[-2:])+link.attrs['href'])
+                urls_news_part.append("http://cdrb.cdyee.com/html/%s-%s/%s/" % (date[:4],date[5:7],date[-2:]) + link.attrs['href'])
     return urls_news_part
 
 
 
-def getText(url):
+def getText(url,headers):
     '''通过url获取包含新闻主标题、副标题、内容的字典'''
     
-    bsObj = getbsObj(url)
-    if bsObj!= None:
+    bsObj = getbsObj(url,headers)
+    if bsObj != None:
         title_one_news = bsObj.find("td",{"class":"bt1"})
         title_two_news = bsObj.find("td",{"class":"bt2"})
         content_news = bsObj.find("td",{"class":"xilan_content_tt"})
-    #news = {'link':url,'bt1':title_one_news.get_text(),'bt2':title_two_news.get_text(),'content':content_news.get_text()}
+    else:
+        return {'link':'','bt1':'','bt2':'','body':''}
+    #news =
+    #{'link':url,'bt1':title_one_news.get_text(),'bt2':title_two_news.get_text(),'content':content_news.get_text()}
     news = {'link':url,'bt1':title_one_news,'bt2':title_two_news,'body':content_news}
     sleep(0.5)
     return news
 
-def findWithDate(date,urls_ban,urls_news):
+def findWithDate(date,urls_ban,urls_news,headers):
     '''通过日期获取所有新闻的链接'''
    
 
     for i in range(0,10):
-        i = "http://cdrb.cdyee.com/html/%s-%s/%s/node_%d.htm" % (date[:4],date[4:6],date[-2:],i)
-        urls_ban.append(i)
+        url_temp = "http://cdrb.cdyee.com/html/%s-%s/%s/node_%d.htm" % (date[:4],date[5:7],date[-2:],i)
+        urls_ban.append(url_temp)
     for url in urls_ban.copy():
-        if url !=None:
-            urls_news+= getLinksFrom_Ban(date,url)
+        if url != None:
+            urls_news+= getLinksFrom_Ban(date,url,headers)
     return urls_news
 
-def getbsObj(url):
+def getbsObj(url,headers):
     '''验证url是否可用，并返回bs对象'''
     try:    
         html = urlopen(url)
@@ -53,66 +57,69 @@ def getbsObj(url):
         print("%s无法连接，继续下一个" % url)
         return None
     try:
-        bsObj = BeautifulSoup(html)
+        session = requests.Session()
+        req = session.get(url,headers = headers)
+        bsObj = BeautifulSoup(req.text,"lxml")
     except AttributeError as e:
         return None
     return bsObj
 
 def typeDate(news):
-    news_typed ={}
-    news_typed['link'] =news['link']
-    news_typed['bt1'] =news['bt1'].get_text()
-    if news['bt2']!='':
-        news_typed['bt2'] =news['bt2'].get_text()
+    news_typed = {}
+    news_typed['link'] = news['link']
+    news_typed['bt1'] = news['bt1'].get_text()
+    if news['bt2'] != '':
+        news_typed['bt2'] = news['bt2'].get_text()
     else:
-        news_typed['bt2'] =''
+        news_typed['bt2'] = ''
 
-    news_typed['body'] =news['body'].get_text().replace('\xa0',' ')
+    news_typed['body'] = news['body'].get_text().replace('\xa0',' ')
     return news_typed
     
 
 
 def saveToTxt(news,filename):
-    with open(filename,'a') as f_obj:
-        f_obj.write(str(news['link'])+'\n')
-        f_obj.write(str(news['bt1'])+'\n')
-        f_obj.write(str(news['bt2'])+'\n')
-        f_obj.write(str(news['body'])+'\n')
+    with open(filename,'a',errors='ignore') as f_obj:
+        
+        f_obj.write(str(news['link']) + '\n')
+        f_obj.write(str(news['bt1']) + '\n')
+        f_obj.write(str(news['bt2']) + '\n')
+        f_obj.write(str(news['body']) + '\n')
         f_obj.write('\n')
-
+        
 def saveToJson(list,filename):
     with open(filename,'w') as f:
-        json.dump(list,f)
+        f.write(json.dumps(list,ensure_ascii=False)+"\n")
 
 
 def date_add(date):
-    new_date = datetime.date(int(date[:4]),int(date[4:6]),int(date[-2:]))+datetime.timedelta(days = 1)
-    new_date_formated = time.strftime('%Y%m%d',time.strftime(new_date))
+    new_date = datetime.datetime.strptime(date,"%Y-%m-%d") + datetime.timedelta(days = 1)
+    new_date_formated = datetime.datetime.strftime(new_date,'%Y-%m-%d')
     return new_date_formated
 
-def screeningUrl(urls_news,keyword):
+def screeningUrl(urls_news,keyword,headers):
     '''关键词筛选'''
-    newurls =[]
+    newurls = []
     news = {}
     for url in urls_news:
-        news = getText(url)
-        if news['bt1']!=None:
+        news = getText(url,headers)
+        if news['bt1'] != None:
             if keyword in news['bt1']:
                 newurls.append(url)
-            if keyword in news['bt2']and url not in newurls:
+            if keyword in news['bt2'] and url not in newurls:
                 newurls.append(url)
-            if keyword in news['body']and url not in newurls:
+            if keyword in news['body'] and url not in newurls:
                 newurls.append(url)
     newurls = list(set(newurls))
 
     return newurls
 
 
-def getdate(b):
+def getdate():
     while True:
-        r = "[0-9]*8"
-        date_input = input("请输入查询起始日期（例如“20180401”）")
-        if re.match(r,date_input)and is_valid_date(date_input[:4]+'-'+date_input[4:6]+'-'+date_input[-2:]):
+        r = "(([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8]))))|((([0-9]{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00))-02-29)"
+        date_input = input("请输入查询起始日期（例如“2018-04-01”）")
+        if re.match(r,date_input):
             return date_input
             break
         
@@ -121,7 +128,7 @@ def getdays():
    while True:
         r = "[0-9]*3"
         days = input("请输入小于300的整数")
-        if re.match(r,days)and int(days)<=300and int(days)>0:
+        if re.match(r,days) and int(days) <= 300 and int(days) > 0:
             return int(days)
         else:
             continue
@@ -134,4 +141,7 @@ def is_valid_date(str):
     except:
         return False        
 
+def make_path(filename,link):
+    newfilename = str.replace(link[str.find(link,'l/') + 2:str.find(link,'l/') + 12],'/','-')+filename
+    return newfilename
  
